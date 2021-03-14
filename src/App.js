beba2c744f64411426594cf74c12f7d699904237
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-03-14 23:56:39 +0200

    adds localStorage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,24 @@ import Filter from './components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   deleteContact = contactid => {
     this.setState(prevState => ({
@@ -58,11 +68,6 @@ class App extends Component {
         number.includes(normalizedFilter),
     );
   };
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevState);
-    console.log(this.state);
-  }
 
   render() {
     const { filter } = this.state;
